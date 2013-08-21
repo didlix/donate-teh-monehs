14fdedd4a1ca0f5b88db7aa1acae7c3488571fad
Rachel Graves <rachel.graves@moneyadviceservice.org.uk>
2013-08-21 19:37:09 +0100

    Add Rails
    @maartenjacobs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EventeeeIoioio::Application.config.secret_token = 'b32037a8aa2b1843ceb9631137bf5b77cd93b5d49ffd0358be48482195f63a5796f9be2dc968cba84d3cb3f08e581e0028e4e6f84d363b0125ae4bc396f4d511'
